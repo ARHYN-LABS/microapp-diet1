5af3313c9391291d280b9618bd5578c08ff9b4f4
Mubashir Iqbal <miqbal@dccdialysis.com>
2026-01-28 20:17:39 +0500

    Adjust drawer logo padding

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -257,7 +257,7 @@ function DrawerContent({ navigation }: { navigation: any }) {
         paddingBottom: 24
       }}
     >
-      <View style={{ marginBottom: 28 }}>
+      <View style={{ marginBottom: 28, paddingLeft: 6 }}>
         <Image
           source={require("./assets/drawer-logo.png")}
           style={{ width: 200, height: 88, resizeMode: "contain" }}
